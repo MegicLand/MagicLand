9854c97ffedbbf298628e605f031604efc7e2acf
Lumin <sweet_0424@qq.com>
2017-05-20 00:36:13 +0800

    create MagicBase

---
+++ Assets/MagicLand/Scripts/Magic/MagicBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBase : MonoBehaviour {
+	public Vector3 startPos;
+	public Vector3 targetPos;
+	public float magicExpend;
+	public float damage;
+	public int lifeTime;
+
+	public void EndMagic()
+	{
+		GameObject.FindGameObjectWithTag(Tags.player).GetComponent<PlayerBehavior>().ToDrawRuneState();
+	}
+}
